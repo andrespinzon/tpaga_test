89986268d56f0c372cc5bd11706cbdde3d4237d5
andres <inge.andrespinzon@gmail.com>
2020-10-20 17:39:30 -0500

    Feat | add methods for field validations

--- wallet/views/transactions.py
+++ wallet/views/transactions.py
@@ -1,17 +1,18 @@
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.response import Response
 
 
 @api_view(['POST'])
-def create_payment(request) -> Response:
+def create_payment(request: Request) -> Response:
     pass
 
 
 @api_view(['GET'])
-def get_purchase_details(request) -> Response:
+def get_purchase_details(request: Request) -> Response:
     pass
 
 
 @api_view(['GET'])
-def list_transactions(request):
+def list_transactions(request: Request) -> Response:
     pass
